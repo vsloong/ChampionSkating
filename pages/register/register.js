15bafae46315b62a3d69c0d0ccfef95135b5d5ac
HandsomeDragon_Wu <742420210@qq.com>
2018-05-24 17:36:22 +0800

    添加认证功能

--- pages/register/register.js
+++ pages/register/register.js
@@ -4,7 +4,9 @@ Page({
   data: {
     avatar: "",
     nickname: "",
-    grand: ""
+    grand: "",
+    address: "",
+    contact: ""
   },
 
   onLoad: function (options) {
@@ -13,4 +15,43 @@ Page({
     })
   },
 
+  chooseLocation: function () {
+    var self = this
+    if (this.data.address) {
+      this.setData({
+        address: ""
+      })
+    } else {
+      wx.chooseLocation({
+        success: function (res) {
+          console.log("获取位置信息成功：" + JSON.stringify(res))
+          self.setData({
+            address: res.address
+          })
+        },
+        fail: function (res) {
+          console.log("获取位置信息失败：" + JSON.stringify(res))
+        }
+      })
+    }
+
+  },
+
+  locationQuestion: function () {
+    wx.showModal({
+      title: '注册位置信息需知',
+      content: '1、完成注册后可以在“附近玩轮滑的人”中查看到自己的定位信息；\r\n2、如果认证了教练的话那么周围的人有意愿学习轮滑就可以直接联系你啦',
+      showCancel: false,
+      confirmText: "我知道了"
+    })
+  },
+
+  contactQuestion: function () {
+    wx.showModal({
+      title: '注册联系方式需知',
+      content: '注册自己的联系方式信息后附近和你一起志同道合的小伙伴就可以很方便的联系你了',
+      showCancel: false,
+      confirmText: "我知道了"
+    })
+  }
 })
