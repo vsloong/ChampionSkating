cdfcb69d328e24dc3ddbfcb6eaddc5ea1bda8ad5
HandsomeDragon_Wu <742420210@qq.com>
2018-05-25 14:23:22 +0800

    添加注册后台交互

--- pages/register/register.js
+++ pages/register/register.js
@@ -6,7 +6,12 @@ Page({
     nickname: "",
     grand: "",
     address: "",
-    contact: ""
+    contact: "",
+    focus: false,
+
+    showContact: false,    //控制联系方式的隐藏
+    showAddress: false,   //控制地址栏的隐藏
+    showRegister: false   //控制认证按钮的隐藏
   },
 
   onLoad: function (options) {
@@ -34,12 +39,57 @@ Page({
         }
       })
     }
+  },
 
+  showContact: function () {
+    this.setData({
+      focus: !this.data.focus
+    })
+  },
+
+  getUserInfo: function () {
+    wx.getUserInfo({
+      withCredentials: true,
+      lang: '',
+      success: function (res) {
+        console.log("获取微信数据成功：" + JSON.stringify(res))
+        // console.log( JSON.stringify(res.userInfo))
+      },
+      fail: function (res) {
+        console.log("获取微信数据失败：" + JSON.stringify(res))
+      },
+      complete: function (res) {
+        // console.log("获取微信数据完成：" + JSON.stringify(res))
+      },
+    })
+  },
+
+  register: function () {
+    if (this.data.showRegister)
+      wx.login({
+        success: function (res) {
+          //console.log("获取微信登录数据成功：" + JSON.stringify(res.code))
+          wx.request({
+            method: "POST",
+            url: 'https://xdrqojro.qcloud.la/weapp/skating/register', //仅为示例，并非真实的接口地址
+            data: {
+              code: res.code,
+              y: '哇哈哈'
+            },
+            header: {
+              'content-type': 'application/json' // 默认值
+            },
+            success: function (res) {
+              console.log(res.data)
+            }
+          })
+        }
+      })
   },
 
   locationQuestion: function () {
     wx.showModal({
-      title: '注册位置信息需知',
+      title: '展示位置信息需知',
       content: '1、完成注册后可以在“附近玩轮滑的人”中查看到自己的定位信息；\r\n2、如果认证了教练的话那么周围的人有意愿学习轮滑就可以直接联系你啦',
       showCancel: false,
       confirmText: "我知道了"
@@ -48,8 +98,8 @@ Page({
 
   contactQuestion: function () {
     wx.showModal({
-      title: '注册联系方式需知',
-      content: '注册自己的联系方式信息后附近和你一起志同道合的小伙伴就可以很方便的联系你了',
+      title: '展示联系方式需知',
+      content: '展示自己的联系方式信息后附近和你一起志同道合的小伙伴就可以很方便的联系你了',
       showCancel: false,
       confirmText: "我知道了"
     })
