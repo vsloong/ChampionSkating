6333ab1d21359c596836ccff10982571cb1864a4
HandsomeDragon_Wu <742420210@qq.com>
2018-07-04 16:20:44 +0800

    添加空视图

--- pages/job/job.js
+++ pages/job/job.js
@@ -1,13 +1,16 @@
 Page({
 
   data: {
+    list: [],
     club: "冠军轮滑培训机构",
     user: "武教练",
     content: "西湖区诚聘晚轮滑初中高级教练"
   },
 
   onLoad: function(options) {
-
+    wx.setNavigationBarTitle({
+      title: "兼职信息"
+    })
   },
 
   goNavigation: function() {
