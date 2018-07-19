f02018f813481375608e5e17f2a9fb5c08167803
HandsomeDragon_Wu <742420210@qq.com>
2018-07-19 17:38:38 +0800

    发布信息页面

--- pages/job/job.js
+++ pages/job/job.js
@@ -1,11 +1,9 @@
 Page({
 
   data: {
+    hidePermissionModal: true,
+    permissionValue: "",
     list: [],
-    title: "兼职",
-    club: "冠军轮滑培训机构",
-    user: "武教练",
-    content: "西湖区诚聘晚轮滑初中高级教练",
   },
 
   onLoad: function(options) {
@@ -28,9 +26,72 @@ Page({
 
 
   goPublish: function() {
-    wx.navigateTo({
-      url: '../publish/publish',
-    })
-  }
+    //先验证是否有权限 1、是否认证；2是否获取到发布密钥
+    var openid = wx.getStorageSync(getApp().data.key_openid)
 
+    //1、判断是否认证
+    if (!openid) {
+      wx.showModal({
+        title: '温馨提示',
+        content: '抱歉！您还没有认证，请提交认证后再发布信息',
+        showCancel: false,
+        confirmText: "我知道了"
+      })
+      return
+    }
+
+    //2、输入从客服处获取的密钥信息
+    this.setData({
+      hidePermissionModal: false
+    })
+  },
+
+
+  //权限验证对话框取消按钮
+  permissionCancel: function() {
+    this.setData({
+      hidePermissionModal: true
+    });
+  },
+
+  //权限验证对话框确认按钮
+  permissionConfirm: function() {
+    this.setData({
+      hidePermissionModal: true
+    })
+
+    wx.showModal({
+      title: '温馨提示',
+      content: '您输入的密钥值不合法',
+      confirmText: '我知道了',
+      showCancel: false
+    })
+
+    // if (this.data.permissionValue == "1234567890") {
+    //   //清空原来的密钥信息
+    //   this.setData({
+    //     permissionValue: ""
+    //   })
+    //   //确认有权限才可以发布
+    //   wx.navigateTo({
+    //     url: '../publish/publish',
+    //   })
+    // } else {
+    //   wx.showModal({
+    //     title: '温馨提示',
+    //     content: '您输入的密钥值不合法',
+    //     confirmText: '我知道了',
+    //     showCancel: false
+    //   })
+    // }
+  },
+
+  bindInput: function(input) {
+    console.log("输入内容：" + JSON.stringify(input.detail.value))
+    var value = input.detail.value
+
+    if (value.length == 10) {
+      this.data.permissionValue = value
+    }
+  }
 })
--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -6,9 +6,21 @@ Page({
 
   onLoad: function(options) {
 
-    var title = "发布招聘信息"//发布活动信息
+    var title = "发布招聘信息" //发布活动信息
     wx.setNavigationBarTitle({
       title: title,
     })
   },
+
+  chooseType: function() {
+    wx.showActionSheet({
+      itemList: ['教练', '销售', '主管'],
+      success: function(res) {
+        console.log("成功结果：" + JSON.stringify(res))
+      },
+      fail: function(res) {
+        console.log("失败结果：" + JSON.stringify(res))
+      }
+    })
+  }
 })
