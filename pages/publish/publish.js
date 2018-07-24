667d3ab624065cd10c9b66c6529f8e9e660d6238
HandsomeDragon_Wu <742420210@qq.com>
2018-07-24 17:32:06 +0800

    时间选择器

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -1,8 +1,11 @@
 Page({
 
   data: {
-    chooseSize: false,
-    animationData: {}
+    showModal: true,
+    dates: '2016-11-08',
+    times: '00:00',
+    objectArray: ['中国', '英国', '美国'],
+    index: 0,
   },
 
   onLoad: function(options) {
@@ -24,53 +27,18 @@ Page({
       }
     })
   },
-  chooseSezi: function(e) {
-    // 用that取代this，防止不必要的情况发生
-    var that = this;
-    // 创建一个动画实例
-    var animation = wx.createAnimation({
-      // 动画持续时间
-      duration: 200,
-      // 定义动画效果，当前是匀速
-      timingFunction: 'linear'
-    })
-    // 将该变量赋值给当前动画
-    that.animation = animation
-    // 先在y轴偏移，然后用step()完成一个动画
-    animation.translateY(200).step()
-    // 用setData改变当前动画
-    that.setData({
-      // 通过export()方法导出数据
-      animationData: animation.export(),
-      // 改变view里面的Wx：if
-      chooseSize: true
-    })
-    // 设置setTimeout来改变y轴偏移量，实现有感觉的滑动
-    setTimeout(function() {
-      animation.translateY(0).step()
-      that.setData({
-        animationData: animation.export()
-      })
-    }, 200)
-  },
-  hideModal: function(e) {
-    var that = this;
-    var animation = wx.createAnimation({
-      duration: 1000,
-      timingFunction: 'linear'
-    })
-    that.animation = animation
-    animation.translateY(200).step()
-    that.setData({
-      animationData: animation.export()
 
+  chooseTime: function() {
+    this.setData({
+      showModal: false
     })
-    setTimeout(function() {
-      animation.translateY(0).step()
-      that.setData({
-        animationData: animation.export(),
-        chooseSize: false
-      })
-    }, 200)
-  }
+  },
+  //  点击时间组件确定事件  
+  bindTimeChange: function(e) {
+    console.log("时间选择完毕：" + e.detail.value)
+    this.setData({
+      times: e.detail.value
+    })
+  },
+
 })
